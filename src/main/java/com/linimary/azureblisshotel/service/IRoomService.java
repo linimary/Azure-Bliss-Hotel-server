f13b0d08823ab1d535c47ab6347ca397c398c0d0
linimary <mariya.lichanova@gmail.com>
2024-04-11 17:20:18 +0300

    Added a new endpoint for searching available rooms

--- src/main/java/com/linimary/azureblisshotel/service/IRoomService.java
+++ src/main/java/com/linimary/azureblisshotel/service/IRoomService.java
@@ -6,6 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,4 +25,5 @@ public interface IRoomService {
 
     Optional<Room> getRoomById(Long roomId);
 
+    List<Room> getAvailableRooms(LocalDate checkInDate, LocalDate checkOutDate, String roomType);
 }
